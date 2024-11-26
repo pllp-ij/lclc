2efaab7dd181dc5f4b114874d526b1f842256d9c
Ano <ano@email.com>
2024-11-25 18:15:03 -0800

    add <remove-element>

---
+++ explore/arrays101/introduction/remove-element/method1-extra-array/RemoveElementV1.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+/**
+    VARS:
+        val(int): the target value of number in initial array
+        nonValNum(int): the number of non-val in initial array
+        nonValNewArr(int[]): a new array containing all non-val elements
+    DESCRIPTION:
+        STEP 1
+        Initialize nonValNewArr with the same size of initial array
+        Initialize nonValNum to 0
+        STEP 2
+        Iterate each number in initial array,
+            If current number not equals to val,
+                STEP 3
+                Push current non-val number to nonValNewArr, and increase nonValNum by one                
+        STEP 4
+        Copy nonValNewArr back to initial array for the first nonValNum elements
+        STEP 5
+        Return nonValNum
+    TIME:
+        O(n)
+    SPACE:
+        O(n), extra space for non-val elements
+    TO BE OPTIMIZED:
+        Avoid using extra space
+*/
+
+class RemoveElementV1 {
+    
+    public static int removeElement(int[] nums, int val) {
+        // STEP 1
+        int[] nonValNewArr = new int[nums.length];
+        int nonValNum = 0;
+        // STEP 2
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                // STEP 3
+                nonValNewArr[nonValNum++] = nums[i];
+            }
+        }
+        // STEP 4
+        for (int i = 0; i < nonValNum; i++) {
+            nums[i] = nonValNewArr[i];
+        }
+        // STEP 5
+        return nonValNum;
+    }
+    
+    public static void printArrRange(int[] nums, int start, int len) {
+            if (len < 0) {
+                return;
+            }
+            System.out.print("[" + nums[start]);
+            for (int i = start + 1; i <= start + len - 1; i++) {
+                System.out.print(", " + nums[i]);
+            }
+            System.out.println("]\n");
+    }
+    
+    public static void main(String[] args) {
+        // int[] nums = {3, 2, 2, 3};
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println("Before: " + Arrays.toString(nums));
+        int k = removeElement(nums, 2);
+        System.out.println("After: ");
+        printArrRange(nums, 0, k);
+        
+    }
+}
---
+++ explore/arrays101/introduction/remove-element/method2-in-place/RemoveElementV1.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+/**
+    VARS:
+        idxInInitArray(int): iteration index in initial array
+        valNum(int): record the number of val in initial array until current iteration
+    DESCRIPTION:
+        STEP 1
+        Initialize idxInInitArray to 0
+        Initialize valNum to 0
+        STEP 2
+        Iterate each index(i) in initial array
+            If current number equals to val,
+                STEP 3
+                Increase valNum by one
+            Else, (meaning non-val elements)
+                STEP 4
+                Copy current number to (i - valNum) position
+        STEP 5
+        Return non-val elements number
+            return (idxInInitArray - valNum)
+        
+    TIME:
+        O(n)
+    SPACE:
+        O(1)
+*/
+
+class RemoveElementV1 {
+    
+    public static int removeElement(int[] nums, int val) {
+        // STEP 1
+        int idxInInitArray = 0;
+        int valNum = 0;
+        // STEP 2
+        for (; idxInInitArray < nums.length; idxInInitArray++) {
+            if (nums[idxInInitArray] == val) {
+                // STEP 3
+                valNum++;
+            } else {
+                // STEP 4
+                nums[idxInInitArray - valNum] = nums[idxInInitArray];
+            }
+        }
+        // STEP 5
+        return idxInInitArray - valNum;
+    }
+    
+    public static void printArrRange(int[] nums, int start, int len) {
+            if (len < 0) {
+                return;
+            }
+            System.out.print("[" + nums[start]);
+            for (int i = start + 1; i <= start + len - 1; i++) {
+                System.out.print(", " + nums[i]);
+            }
+            System.out.println("]\n");
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        // int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println("Before: " + Arrays.toString(nums));
+        int k = removeElement(nums, 2);
+        System.out.println("After: ");
+        printArrRange(nums, 0, k);
+        
+    }
+}
